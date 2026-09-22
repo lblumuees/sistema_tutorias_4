98165515d492a2ecb320975cba417dc11cf03363
Luis <luis.blum@uees.edu.ec>
2026-09-21 23:01:01 -0500

    Correcciones main

--- src/main.py
+++ src/main.py
@@ -1,17 +1,18 @@
-from Estudiante import Estudiante
-from Docente import Docente
-from Tutoria import Tutoria
-from Reserva import Reserva
-from Notificador import Notificador, NotificadorCorreo, NotificadorCalendario, NotificadorPanel
-from CancelacionStrategy import CancelacionNormal, CancelacionPrioritaria, CancelacionGrupal
-from ServicioReservas import ServicioReservas
+from src.Estudiante import Estudiante
+from src.Docente import Docente
+from src.Tutoria import Tutoria
+from src.Reserva import Reserva
+from src.Notificador import Notificador, NotificadorCorreo, NotificadorCalendario, NotificadorPanel
+from src.CancelacionStrategy import CancelacionNormal, CancelacionPrioritaria, CancelacionGrupal
+from src.ServicioReservas import ServicioReservas
 from datetime import datetime
 
 def main():
     # objetos base
     estudiante = Estudiante(1, "Luis", "luis@mail.com")
     docente = Docente(1, "Prof. Jaime")
-    tutoria = Tutoria(1, "Diseño de Software", "08:00 PM", docente)
+    #tutoria = Tutoria(1, "Diseño de Software", "08:00 PM", docente)
+    tutoria = Tutoria("Diseño de Software")
 
     # nueva reserva
     reserva = Reserva(1, estudiante, tutoria,"08/09/2026")
@@ -27,17 +28,17 @@ def main():
 
     # aplicar diferentes formas de cancelación (Strategy)
     print("\n--- Cancelación con estrategia normal ---")
-    servicio_normal = ServicioReservas(CancelacionNormal())
+    servicio_normal = ServicioReservas(CancelacionNormal(), NotificadorCalendario())
     servicio_normal.cancelar_reserva(reserva)
     print(f"Estado actual: {reserva.estado}")
 
     print("\n--- Cancelación con estrategia prioritaria ---")
-    servicio_prioritaria = ServicioReservas(CancelacionPrioritaria())
+    servicio_prioritaria = ServicioReservas(CancelacionPrioritaria(), NotificadorCalendario())
     servicio_prioritaria.cancelar_reserva(reserva)
     print(f"Estado actual: {reserva.estado}")
 
     print("\n--- Cancelación con estrategia grupal ---")
-    servicio_grupal = ServicioReservas(CancelacionGrupal())
+    servicio_grupal = ServicioReservas(CancelacionGrupal(), NotificadorCalendario())
     servicio_grupal.cancelar_reserva(reserva)
     print(f"Estado actual: {reserva.estado}")
 
