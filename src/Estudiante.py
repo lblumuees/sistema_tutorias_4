76a277331e3b2c030961ce15cc629dbda669c2f5
Luis <luis.blum@uees.edu.ec>
2026-09-21 22:05:54 -0500

    Refactorización: introducir Value Object Correo en clase Estudiante

--- src/Estudiante.py
+++ src/Estudiante.py
@@ -1,8 +1,18 @@
+class Correo:
+    def __init__(self, valor: str):
+        if valor is None or "@" not in valor:
+            raise ValueError("Correo inválido")
+        self.valor = valor
+
+    def __str__(self):
+        return self.valor
+
+
 class Estudiante:
     def __init__(self, id, nombre, email):
         self.id = id
         self.nombre = nombre
-        self.email = email
+        self.email = Correo(email)   # Value Object
 
     def solicitar_tutoria(self, tutoria):
         print(f"{self.nombre} solicita la tutoría {tutoria.tema}")
